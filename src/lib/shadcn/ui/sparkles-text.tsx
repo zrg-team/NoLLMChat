ba278ae0c9427cb97573ccedf11021ccb793d7c8
Hung Duong Tan <zerglingno2@outlook.com>
2024-11-24 13:42:48 +0700

    chore: format code

--- src/lib/shadcn/ui/sparkles-text.tsx
+++ src/lib/shadcn/ui/sparkles-text.tsx
@@ -60,70 +60,73 @@ interface SparklesTextProps {
   }
 }
 
-const SparklesText: React.FC<SparklesTextProps> = memo(({
-  text,
-  colors = { first: '#9E7AFF', second: '#FE8BBB' },
-  className,
-  sparklesCount = 10,
-  ...props
-}) => {
-  const [sparkles, setSparkles] = useState<Sparkle[]>([])
+const SparklesText: React.FC<SparklesTextProps> = memo(
+  ({
+    text,
+    colors = { first: '#9E7AFF', second: '#FE8BBB' },
+    className,
+    sparklesCount = 10,
+    ...props
+  }) => {
+    const [sparkles, setSparkles] = useState<Sparkle[]>([])
 
-  useEffect(() => {
-    const generateStar = (): Sparkle => {
-      const starX = `${Math.random() * 100}%`
-      const starY = `${Math.random() * 100}%`
-      const color = Math.random() > 0.5 ? colors.first : colors.second
-      const delay = Math.random() * 2
-      const scale = Math.random() * 1 + 0.3
-      const lifespan = Math.random() * 10 + 5
-      const id = `${starX}-${starY}-${Date.now()}`
-      return { id, x: starX, y: starY, color, delay, scale, lifespan }
-    }
-
-    const initializeStars = () => {
-      const newSparkles = Array.from({ length: sparklesCount }, generateStar)
-      setSparkles(newSparkles)
-    }
-
-    const updateStars = () => {
-      setSparkles((currentSparkles) =>
-        currentSparkles.map((star) => {
-          if (star.lifespan <= 0) {
-            return generateStar()
-          } else {
-            return { ...star, lifespan: star.lifespan - 0.1 }
-          }
-        }),
-      )
-    }
-
-    initializeStars()
-    const interval = setInterval(updateStars, 100)
-
-    return () => clearInterval(interval)
-  }, [colors.first, colors.second])
-
-  return (
-    <div
-      className={cn('text-6xl font-bold', className)}
-      {...props}
-      style={
-        {
-          '--sparkles-first-color': `${colors.first}`,
-          '--sparkles-second-color': `${colors.second}`,
-        } as CSSProperties
+    useEffect(() => {
+      const generateStar = (): Sparkle => {
+        const starX = `${Math.random() * 100}%`
+        const starY = `${Math.random() * 100}%`
+        const color = Math.random() > 0.5 ? colors.first : colors.second
+        const delay = Math.random() * 2
+        const scale = Math.random() * 1 + 0.3
+        const lifespan = Math.random() * 10 + 5
+        const id = `${starX}-${starY}-${Date.now()}`
+        return { id, x: starX, y: starY, color, delay, scale, lifespan }
       }
-    >
-      <span className="relative inline-block">
-        {sparkles.map((sparkle) => (
-          <Sparkle key={sparkle.id} {...sparkle} />
-        ))}
-        <strong>{text}</strong>
-      </span>
-    </div>
-  )
-}, (pre, next) => pre.text === next.text)
+
+      const initializeStars = () => {
+        const newSparkles = Array.from({ length: sparklesCount }, generateStar)
+        setSparkles(newSparkles)
+      }
+
+      const updateStars = () => {
+        setSparkles((currentSparkles) =>
+          currentSparkles.map((star) => {
+            if (star.lifespan <= 0) {
+              return generateStar()
+            } else {
+              return { ...star, lifespan: star.lifespan - 0.1 }
+            }
+          }),
+        )
+      }
+
+      initializeStars()
+      const interval = setInterval(updateStars, 100)
+
+      return () => clearInterval(interval)
+    }, [colors.first, colors.second])
+
+    return (
+      <div
+        className={cn('text-6xl font-bold', className)}
+        {...props}
+        style={
+          {
+            '--sparkles-first-color': `${colors.first}`,
+            '--sparkles-second-color': `${colors.second}`,
+          } as CSSProperties
+        }
+      >
+        <span className="relative inline-block">
+          {sparkles.map((sparkle) => (
+            <Sparkle key={sparkle.id} {...sparkle} />
+          ))}
+          <strong>{text}</strong>
+        </span>
+      </div>
+    )
+  },
+  (pre, next) => pre.text === next.text,
+)
 
 const Sparkle: React.FC<Sparkle> = ({ id, x, y, color, delay, scale }) => {
   return (
