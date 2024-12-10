23055e68623f6c75c212291c9b4d9cb7a81d77b8
Hung Duong Tan <zerglingno2@outlook.com>
2024-12-10 20:38:56 +0700

    Updates

--- assets/minus-3wNmBFjY.js
+++
@@ -1,1 +0,0 @@
-import{c as a,__tla as r}from"./createLucideIcon-DJpQymby.js";import{__tla as _}from"./index-DdDyhOp4.js";let t,l=Promise.all([(()=>{try{return r}catch{}})(),(()=>{try{return _}catch{}})()]).then(async()=>{t=a("Minus",[["path",{d:"M5 12h14",key:"1ays0h"}]])});export{l as __tla,t as default};
